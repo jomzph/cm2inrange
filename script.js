ed68b4628fccfd0ba8419a4ae37ad8cc7f7b49b8
Joemar M <joemarmenesnew@gmail.com>
2022-12-18 23:09:29 +0800

    Add Feature
    Computes shipping and make Notes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
     let one = document.getElementById("una");
     let two = document.getElementById("dalawa");
-
+    let onecjd = document.getElementById("ship1");
+    let twocjd = document.getElementById("ship2");
     
     
     // Enter trigger
@@ -21,8 +22,22 @@ document.getElementById("myButton").onclick = function(){
     document.getElementById("sulat").innerHTML = `${convert1}" - ${convert2}"`;
 
  }
+  // button Click Trigger for CJD
+ document.getElementById("cjd").onclick = function(){
 
+  var convert1 = parseFloat(onecjd.value);
+  var convert2 = parseFloat(twocjd.value);
 
+  document.getElementById("cjdshipping").innerHTML = "Shipping Fee: " + `$${convert1} \n Service Fee: $${convert2} \n Total: $`+ (convert1+convert2).toFixed(2);
+
+}
+    // Enter trigger for CJD
+    twocjd.addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("cjd").click();
+      }
+    });
 
 //  function addTable() {
 //     const input = document.createElement("input"); 
